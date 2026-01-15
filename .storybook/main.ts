a00c5e5a5b3d4260947fd12647bc7cf098237bfd
gogo1414 <wnstn6945@gmail.com>
2026-01-15 23:18:11 +0900

    chore: Storybook 설정 및 의존성 추가

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/nextjs-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": "@storybook/nextjs",
+  "staticDirs": [
+    "../public"
+  ]
+};
+export default config;
